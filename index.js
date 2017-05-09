21bfae2d6f0c87e743a389063ffa540b182d5e4c
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-09 19:57:23 +0700

    server receive private message

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.set('views', './views');
 app.get('/', (req, res) => res.render('home'));
 
 const arrUsername = [];
+const arrSocket = [];
 
 io.on('connection', socket => {
     socket.on('DANG_KY_USERNAME', username => {
@@ -20,6 +21,7 @@ io.on('connection', socket => {
         socket.username = username;// eslint-disable-line
         socket.emit('XAC_NHAN_DANG_KY', arrUsername);
         arrUsername.push(username);
+        arrSocket.push(socket);
         io.emit('NGUOI_DUNG_MOI', username);
     });
 
@@ -27,10 +29,17 @@ io.on('connection', socket => {
         io.emit('TIN_NHAN_MOI', `${socket.username}: ${message}`);
     });
 
+    socket.on('TIN_NHAN_RIENG', data => {
+        const { receiver, message } = data;
+        const index = arrSocket.findIndex(e => e.username === receiver);
+        socket.to(arrSocket[index].id).emit('NHAN_TIN_NHAN_RIENG', message);
+    });
+
     socket.on('disconnect', () => {
         const index = arrUsername.indexOf(socket.username);
         if (index !== -1) {
             arrUsername.splice(index, 1);
+            arrSocket.splice(index, 1);
             io.emit('NGUOI_DUNG_THOAT', socket.username);
         }
     });
