e696db5b00719bc163b81e520e3da900265b5520
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-11 19:34:55 +0700

    init

--- public/bundle.js
+++ public/bundle.js
@@ -6322,8 +6322,18 @@ const p = new Peer({
 p.on('signal', 
     data => $('#connectId').text(JSON.stringify(data)));
 
-$(document).ready(() => {// eslint-disable-line
+p.on('connect', () => {
+  console.log('CONNECT');
+  p.send('whatever' + Math.random());
+});
 
+p.on('data', data => console.log(data.toString()));
+
+$(document).ready(() => {// eslint-disable-line
+    $('#btnConnect').click(() => {
+        const otherId = $('#otherId').val();
+        p.signal(JSON.parse(otherId));
+    });
 });
 
 
